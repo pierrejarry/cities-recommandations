6f6bfc1a866164744a483610256ca6f634c0463a
Pierre Jarry <pierre.jarry82@gmail.com>
2025-03-07 14:39:52 +0100

    Added lazy loading to improve performance on page loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
+import { lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import SearchPage from './layout/pages/SearchPage'
-import CityPage from './layout/pages/CityPage';
 import './App.css'
+
+const SearchPage = lazy(() => import('./layout/pages/SearchPage'));
+const CityPage = lazy(() => import('./layout/pages/CityPage'));
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<SearchPage />} />
-            <Route path="/destination" element={<CityPage />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <SearchPage />
+          </Suspense>
+        } />
+        <Route path="/destination" element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <CityPage />
+          </Suspense>
+        } />
+      </Routes>
     </BrowserRouter>
-);
+  );
 }
 
 export default App
