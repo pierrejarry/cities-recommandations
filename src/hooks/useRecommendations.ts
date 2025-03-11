de4ab3b2e67ae9af501fcd63d9ddf9534dd628ab
Pierre Jarry <pierre.jarry82@gmail.com>
2025-03-11 08:52:20 +0100

    Used base url from env file to fetch restaurants

--- src/hooks/useRecommendations.ts
+++ src/hooks/useRecommendations.ts
@@ -3,9 +3,10 @@ import { ApiResponse, PlaceResult, Tip } from "../utils/recommendationsTypes";
 
 function useRecommendations(city: string) {
     const token = import.meta.env.VITE_FOURSQUARE_API_KEY;
+    const baseUrl = import.meta.env.VITE_FOURSQUARE_BASE_URL;
 
-    const fetchSightseeingSpots = async (): Promise<PlaceResult[]> => {
-        const searchUrl = `https://api.foursquare.com/v3/places/search?query=Restaurant&near=${city}&limit=10`;
+    const fetchRestaurants = async (): Promise<PlaceResult[]> => {
+        const searchUrl = `${baseUrl}/search?query=Restaurant&near=${city}&limit=10`;
 
         // Fetch list of restaurants
         const searchResponse = await fetch(searchUrl, {
@@ -25,8 +26,8 @@ function useRecommendations(city: string) {
         // Fetch details and images for each restaurant
         const places: PlaceResult[] = await Promise.all(
             searchData.results.map(async (place) => {
-                const tipsUrl = `https://api.foursquare.com/v3/places/${place.fsq_id}/tips`;
-                const photosUrl = `https://api.foursquare.com/v3/places/${place.fsq_id}/photos`;
+                const tipsUrl = `${baseUrl}/${place.fsq_id}/tips`;
+                const photosUrl = `${baseUrl}/${place.fsq_id}/photos`;
 
                 // Fetch restaurant details (description)
                 const tipsResponse = await fetch(tipsUrl, {
@@ -55,7 +56,7 @@ function useRecommendations(city: string) {
 
     const cityQuery = useQuery<PlaceResult[], Error>({
         queryKey: ["recommendations", city],
-        queryFn: fetchSightseeingSpots,
+        queryFn: fetchRestaurants,
         retry: 1,
     });
 
